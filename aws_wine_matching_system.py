81dfa97a88d2f786f2278dbb7b83e3949c67c82a
Andrew Cowherd <andrew.cowherd@gmail.com>
2025-09-16 04:18:07 -0400

    Updated gitignore

--- aws_wine_matching_system.py
+++ aws_wine_matching_system.py
@@ -481,9 +481,9 @@ class WineMatchingPipeline:
     """Main pipeline orchestrator"""
     
     def __init__(self, api_config: APIConfig, processing_config: ProcessingConfig):
+        self.data_manager = DataManager(processing_config.database_path)
         self.api = MontgomeryCountyAPI(api_config)
         self.api.data_manager = self.data_manager
-        self.data_manager = DataManager(processing_config.database_path)
         self.matcher = OptimizedWineMatcher(processing_config)
         self.processing_config = processing_config
     
